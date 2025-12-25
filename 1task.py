bc429d56ef0fc08459af2427937594a543edc3fa
dijessym <kononovadilaz@gmail.com>
2025-12-25 18:18:37 +0500

    Update 1task.py

--- 1task.py
+++ 1task.py
@@ -4,7 +4,7 @@ count = 0
 for i in range(len(d) - 4):
     if d[i] == '>' and d[i+1] == '>' and d[i+2] == '-' and d[i+3] == '-' and d[i+4] == '>':
         count += 1
-    elif d[i] == '<' and d[i+1] == '-' and d<<[i+2] == '-' and d[i+3] == '<' and d[i+4] == '<':
+    elif d[i] == '<' and d[i+1] == '-' and d[i+2] == '-' and d[i+3] == '<' and d[i+4] == '<':
         count += 1
 
 print(count)
